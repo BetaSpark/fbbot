e27b9f974133eb0bc50707968ad871f5930968c6
Callum D. Jones <cjdeltacallumjones@gmail.com>
2016-04-12 20:55:38 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 app.get('/webhook/', function (req, res) {
-  if (req.query['hub.verify_token'] === '<lol>') {
+  if (req.query['hub.verify_token'] === 'lol') {
     res.send(req.query['hub.challenge']);
   }
   res.send('Error, wrong validation token');
